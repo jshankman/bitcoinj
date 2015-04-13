238f164c04b6e34d9319526b1caf924c5468eb46
Jake Shankman <jes3cu@virginia.edu>
2015-04-13 17:12:32 -0400

    Update MainNetParams.java
    Changed address header to make all addresses start with P

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -32,7 +32,7 @@ public class MainNetParams extends NetworkParameters {
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1d00ffffL);
         dumpedPrivateKeyHeader = 128;
-        addressHeader = 0;
+        addressHeader = 56; //encodes as P in base 58
         p2shHeader = 5;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         port = 22228; //PointCoin has port 
