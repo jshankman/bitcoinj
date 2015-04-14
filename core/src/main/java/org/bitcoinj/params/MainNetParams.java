7e9e5edbf757756aced55272902390f35c08ba4e
Jake Shankman <jes3cu@virginia.edu>
2015-04-14 14:32:09 -0400

    Update MainNetParams.java
    Changed dnsSeed to safari.bitcoin-class.org (which runs the pointcoin protocol) and checkpoints to match pointcoind (original pointcoin library).

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -56,18 +56,24 @@ public class MainNetParams extends NetworkParameters {
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        checkpoints.put(91722, new Sha256Hash("00000000000271a2dc26e7667f8419f2e15416dc6955e5a6c6cdf3f2574dd08e"));
-        checkpoints.put(91812, new Sha256Hash("00000000000af0aed4792b1acee3d966af36cf5def14935db8de83d6f9306f2f"));
-        checkpoints.put(91842, new Sha256Hash("00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec"));
-        checkpoints.put(91880, new Sha256Hash("00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721"));
-        checkpoints.put(200000, new Sha256Hash("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf"));
+        checkpoints.put(11111, new Sha256Hash("0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d"));
+        checkpoints.put(33333, new Sha256Hash("000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6"));
+        checkpoints.put(74000, new Sha256Hash("0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20"));
+        checkpoints.put(105000, new Sha256Hash("00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97"));
+        checkpoints.put(134444, new Sha256Hash("00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe"));
+        checkpoints.put(168000, new Sha256Hash("000000000000099e61ea72015e79632f216fe6cb33d7899acb35b75c8303b763"));
+        checkpoints.put(193000, new Sha256Hash("000000000000059f452a5f7340de6682a977387c17010ff6e6c3bd83ca8b1317"));
+        checkpoints.put(210000, new Sha256Hash("000000000000048b95347e83192f69cf0366076336c639f9b7228e9ba171342e"));
+        checkpoints.put(216116, new Sha256Hash("00000000000001b4f4b433e81ee46494af945cf96014816a4e2370f11b23df4e"));
+        checkpoints.put(225430, new Sha256Hash("00000000000001c108384350f74090433e7fcf79a606b8e797f065b130575932"));
+        checkpoints.put(250000, new Sha256Hash("000000000000003887df1f29024b06fc2200b55f8af8f35453d7be294df2d214"));
+        checkpoints.put(267300, new Sha256Hash("000000000000000a83fbd660e918f218bf37edd92b748ad940483c7c116179ac"));
+        checkpoints.put(279000, new Sha256Hash("0000000000000001ae8c72a0b0c301f67e3afca10e819efa9041e458e9bd7e40"));
+        checkpoints.put(300255, new Sha256Hash("0000000000000000162804527c6e9b9f0563a280525f9d08c12041def0a0f3b2"));
+		checkpoints.put(319400, new Sha256Hash("000000000000000021c6052e9becade189495d1c539aa37c58917305fd15f13b"));
 
         dnsSeeds = new String[] {
-                "seed.bitcoin.sipa.be",        // Pieter Wuille
-                "dnsseed.bluematt.me",         // Matt Corallo
-                "dnsseed.bitcoin.dashjr.org",  // Luke Dashjr
-                "seed.bitcoinstats.com",       // Chris Decker
-                "seed.bitnodes.io",            // Addy Yeow
+                "safari.bitcoin-class.org",        // Block Explorer
         };
     }
 
